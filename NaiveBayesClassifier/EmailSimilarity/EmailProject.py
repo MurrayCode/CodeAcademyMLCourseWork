289db6f58e0712f981bfa8081784b9a051cd7662
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-10-11 16:35:37 +0100

    NaiveBayesClassifier completed

---
+++ NaiveBayesClassifier/EmailSimilarity/EmailProject.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+train_emails = fetch_20newsgroups(subset='train', shuffle=True, random_state=108)
+test_emails = fetch_20newsgroups(subset='test', shuffle=True, random_state=108)
+#print(emails.target_names)
+counter = CountVectorizer()
+categories = ['comp.sys.ibm.pc.hardware','rec.sport.hockey']
+#print(emails.data[5])
+#print(emails.target[5])
+counter.fit(test_emails.data + train_emails.data)
+
+train_counts = counter.transform(train_emails.data)
+test_counts = counter.transform(test_emails.data)
+
+classifier = MultinomialNB()
+classifier.fit(train_counts, train_emails.target)
+print(classifier.score(test_counts, test_emails.target))
