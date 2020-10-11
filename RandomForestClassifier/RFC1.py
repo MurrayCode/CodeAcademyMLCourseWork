d1aeac796cc053a7b230c6bf039889fcfb7a451c
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-10-11 09:23:03 +0100

    Decision trees and randomForests

---
+++ RandomForestClassifier/RFC1.py
@@ -0,0 +1,20 @@
+from tree import build_tree, print_tree, car_data, car_labels
+import random
+random.seed(4)
+
+tree = build_tree(car_data, car_labels)
+#print_tree(tree)
+
+indices = []
+for i in range(1000):
+  x = random.randint(0,999)
+  indices.append(x)
+
+data_subset = []
+labels_subset = []
+for index in indices:
+  data_subset.append(car_data[index])
+  labels_subset.append(car_labels[index])
+
+subset_tree = build_tree(data_subset, labels_subset)
+print_tree(subset_tree)
